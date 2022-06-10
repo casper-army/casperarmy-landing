41b8eecde3871bed80a867ee589f72e6b34755fc
damiansarnecki <xdamiansarnecki@gmail.com>
2022-06-10 04:20:54 +0200

    fix

--- components/global/Navbar/Menu/launchApp.tsx
+++ components/global/Navbar/Menu/launchApp.tsx
@@ -61,7 +61,6 @@ export const LaunchApp = () => {
         }
       }}
     >
-<<<<<<< HEAD
       {state ? (
         <Grid
           _hover={{ color: "black" }}
@@ -77,14 +76,6 @@ export const LaunchApp = () => {
       ) : (
         "CONNECT WALLET"
       )}
-=======
-      CONNECT&nbsp;WALLET
-      <Box
-        display={{ base: "none", xl: "block" }}
-        bg="#FF0202"
-        boxSize="12px"
-      ></Box>
->>>>>>> b5b88eef0e11c6dbb460ba3cba105b84db62d2c2
     </Flex>
   );
 };
