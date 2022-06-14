3c4517a13b3a7f8f8fa7b101edcac611ba775cc3
CasperArmy <99138733+casperarmy@users.noreply.github.com>
2022-06-14 15:59:13 +0200

    Update layout.tsx

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({ children }: any) => {
   return (
     <>
       <Head>
-        <title>Layouts Example</title>
+        <title>CasperArmy - DAO platform for creators and investors with project incubation HUB and Venture Capital</title>
       </Head>
       <Flex
         overflowX="hidden"
