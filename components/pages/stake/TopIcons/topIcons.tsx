d1eea642b0a0bde00624af931d55de826dd53644
CasperArmy <99138733+casperarmy@users.noreply.github.com>
2022-06-25 17:29:35 +0200

    Update topIcons.tsx

--- components/pages/stake/TopIcons/topIcons.tsx
+++ components/pages/stake/TopIcons/topIcons.tsx
@@ -5,12 +5,12 @@ import { GlassIcon } from "./glassIcon";
 export const TopIcons = () => {
   return <Grid gridTemplateColumns={{base: "1fr", lg: "1fr 1fr"}} gridGap="24px">
 	  <GlassIcon
-	  	content={"Our dedicated server for the project is characterized by a high level of security. Learn more..."}
+	  	content={"Our dedicated server for the project is characterized by a high level of security."}
 		icon={<Image src={IconAssets.server}/>}
 		heading={'Secure server!'}
 	  />
 	  	  <GlassIcon
-	  	content={"We care about the environment, so our servers use renewable energy sources. Read more..."}
+	  	content={"We care about the environment, so our servers use renewable energy sources."}
 		icon={<Image src={IconAssets.server}/>}
 		heading={'We are Eco Friendly!'}
 	  />
