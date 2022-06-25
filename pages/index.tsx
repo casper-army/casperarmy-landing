7db2994358b305d99bcb52acc3164897a1bd7182
CasperArmy <99138733+casperarmy@users.noreply.github.com>
2022-06-25 14:13:27 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { Flex, Box } from "@chakra-ui/layout";
 import { CenterContainer } from "../components/shared/containers/centerContainer";
 import React from "react";
+import { Text } from "react-native-web";
 import { Navbar } from "../components/global/Navbar/navbar";
 import { Image } from "@chakra-ui/image";
 import { Partners } from "../components/pages/index/Partners/partners";
