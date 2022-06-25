f465ab068694047d04beb8c071dff1608a3c4679
CasperArmy <99138733+casperarmy@users.noreply.github.com>
2022-06-25 13:39:55 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,8 +40,7 @@ const Home: NextPage = () => {
         <PageHeader
           addon={<IndexHeadingAddon />}
           description={
-          "A Casper Network community project that unites"
-          "A DAO platform for creators and investors with project incubation HUB and Venture Capital"
+          "A Casper Network community project that unites A DAO platform for creators and investors with project incubation HUB and Venture Capital"
           }
           label={"Welcome to #CasperArmy"}
           heading={<IndexHeading />}
