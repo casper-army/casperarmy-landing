85d239d3199c65c0a2f3cfaa976e54590d05c642
CasperArmy <99138733+casperarmy@users.noreply.github.com>
2022-06-25 14:54:09 +0200

    Update PageHeader.tsx

--- components/shared/PageHeader.tsx
+++ components/shared/PageHeader.tsx
@@ -39,7 +39,7 @@ export const PageHeader: FC<IPageHeader> = ({
         {heading}
       </Heading>
       <Box maxWidth="850px" padding="0px 10px">
-	      <TextBasic><Text>{description}</Text></TextBasic>
+      <Text><TextBasic>{description}</TextBasic></Text>
       </Box>
       <Box>{addon}</Box>
     </Flex>
