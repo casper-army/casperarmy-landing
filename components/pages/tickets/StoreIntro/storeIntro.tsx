1aea09c0be2f3d818be8b1538c7ef9cce85de002
damiansarnecki <xdamiansarnecki@gmail.com>
2022-06-27 01:13:50 +0200

    hide scrollbar

--- components/pages/tickets/StoreIntro/storeIntro.tsx
+++ components/pages/tickets/StoreIntro/storeIntro.tsx
@@ -13,48 +13,48 @@ export const StoreIntro = () => {
         content={"One ticket gives you access to the entire platform"}
         label="Ticket sale rounds"
       />
-      <Flex justify={{base: "initial", xl: "center"}} gap="20px" mb="60px" maxW="100vw" overflowX="scroll" padding="0px 20px" pb="40px">
-        <RoundItem 
-			index={1}
-			price={50}
-			percent={75}
-			max={2000}
-		/>
-		 <RoundItem 
-			index={2}
-			price={125}
-			percent={65}
-			max={1000}
-		/>
-		 <RoundItem 
-			index={3}
-			price={200}
-			percent={43}
-			max={1000}
-		/>
-		 <RoundItem 
-			index={4}
-			price={250}
-			percent={29}
-			max={1000}
-		/>
-		 <RoundItem 
-			index={5}
-			price={300}
-			percent={15}
-			max={1000}
-		/>
-		 <RoundItem 
-			index={6}
-			price={350}
-			percent={75}
-			max={0}
-		/>
-    
+      <Flex
+        justify={{ base: "initial", xl: "center" }}
+        gap="20px"
+        mb="60px"
+        maxW="100vw"
+        overflowX="auto"
+        padding="0px 20px"
+        pb="40px"
+        __css={{
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+          "&::-webkit-scrollbar-track": {
+            display: "none",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            display: "none",
+          },
+        }}
+      >
+        <RoundItem index={1} price={50} percent={75} max={2000} />
+        <RoundItem index={2} price={125} percent={65} max={1000} />
+        <RoundItem index={3} price={200} percent={43} max={1000} />
+        <RoundItem index={4} price={250} percent={29} max={1000} />
+        <RoundItem index={5} price={300} percent={15} max={1000} />
+        <RoundItem index={6} price={350} percent={75} max={0} />
       </Flex>
-      <Grid templateColumns={{base: "1fr", lg: "1fr 1fr"}} gridGap={{base: "40px", md: "120px"}} mb="100px" padding={{base: "20px", lg: "0px"}}>
+      <Grid
+        templateColumns={{ base: "1fr", lg: "1fr 1fr" }}
+        gridGap={{ base: "40px", md: "120px" }}
+        mb="100px"
+        padding={{ base: "20px", lg: "0px" }}
+      >
         <Flex flexDir="column" gap="35px">
-          <Box letterSpacing="0.8em" fontFamily="CHANEY" fontSize="12px" color="#888888">SALES PROCESS</Box>
+          <Box
+            letterSpacing="0.8em"
+            fontFamily="CHANEY"
+            fontSize="12px"
+            color="#888888"
+          >
+            SALES PROCESS
+          </Box>
           <CustomHeader>Simple ticket purchasing interface</CustomHeader>
           <Paragraph>
             Once the countdown to the start of the ticket sale has ended, a
@@ -72,9 +72,8 @@ export const StoreIntro = () => {
           </Box>
         </Flex>
         <Image
-		
           src="https://cdn.discordapp.com/attachments/939935121019117609/990743165445222470/Group_882.png"
-		  maxWidth="80vw"
+          maxWidth="80vw"
         />
       </Grid>
     </CenterContainer>
