a97ec1428d7928c642100c12f229928a7cbfa385
damiansarnecki <xdamiansarnecki@gmail.com>
2022-06-27 20:21:32 +0200

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,10 @@ import Head from "next/head";
 import { Navbar } from "../components/global/Navbar/navbar";
 import "regenerator-runtime/runtime";
 import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const getLayout = (Component as any).getLayout || ((page : any) => page)
-
+  const getLayout = (Component as any).getLayout || ((page: any) => page);
 
   const theme = extendTheme({
     fonts: {
@@ -20,6 +19,21 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ChakraProvider theme={theme}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-61KGRL6TQY`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-61KGRL6TQY', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
